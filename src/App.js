6bf071c2335a32a6498db5827028b897362488f7
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2021-01-21 02:04:54 +0530

    emoji added

--- src/App.js
+++ src/App.js
@@ -2,22 +2,41 @@ import React, {useState} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setcount] = useState(() => {
-    return 4
-  })
+  const [count, setcount] = useState(() => {return 0})
+  const [theme, settheme] = useState('😐')
 
   function decrementCount(){
     setcount(prevCount => prevCount - 1)
+    settheme(prevCount => '🥺')
   }
 
   function incrementCount(){
     setcount(prevCount => prevCount + 1)
+    settheme(prevCount => '😃')
   }
+
+
+  // const [state, setstate] = useState({count:4, theme: 'likes'})
+  // const count = state.count
+  // const theme = state.theme
+
+  // function decrementCount(){
+  //   setstate( prevstate => {
+  //     return{ ...prevstate, count: prevstate.count - 1}
+  //   })
+  // }
+
+  // function incrementCount(){
+  //   setstate(prevstate => {
+  //     return{ ...prevstate, count:prevstate.count + 1}
+  //   })
+  // }
 
   return (
     <div className='box'>
       <button onClick={decrementCount}>-</button>
       <span>{count}</span>
+      <span>{theme}</span>
       <button onClick={incrementCount}>+</button>
     </div>
   )
